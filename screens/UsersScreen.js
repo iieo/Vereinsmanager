1626bbd25f5b8aa211de07ce06ff5845e692ffb8
alcides <leopold.bauer@gmx.de>
2022-03-23 21:22:46 +0100

    modals und databse

---
+++ screens/UsersScreen.js
@@ -0,0 +1,40 @@
+import React, { useContext } from "react";
+import Datatable from "../components/displayData/Datatable";
+import { DataContext } from "../components/database/DatabaseProvider";
+export default function UsersScreen() {
+  const data = useContext(DataContext);
+  const defaultInputs = [
+    { text: "Name", iconName: "person", replacing: "name" },
+    { text: "Vorname", iconName: "person", replacing: "firstName" },
+    { text: "Straße", iconName: "person", replacing: "street" },
+  ];
+  const additionalInputs = [
+    {
+      text: "Mitglied?",
+      iconName: "person",
+      replacing: "member",
+      type: "checkbox",
+    },
+    { text: "Instrument", iconName: "person", replacing: "instrument" },
+  ];
+  return (
+    <Datatable
+      data={data.users.data}
+      title="Personen"
+      fabText="Person hinzufügen"
+      sort={(a, b) => {
+        if (a === b) {
+          return 0;
+        }
+        return a.name < b.name ? -1 : 1;
+      }}
+      headers={defaultInputs}
+      modalData={{
+        defaultInputs,
+        additionalInputs,
+        title: "Person hinzufügen",
+        addItem: data.users.addItem,
+      }}
+    />
+  );
+}
